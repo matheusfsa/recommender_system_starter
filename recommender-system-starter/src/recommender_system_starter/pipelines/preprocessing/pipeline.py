74e0b990c65999e9ff64a18bd43a182c39f6e5ab
Matheus <santosalmeidamatheus@gmail.com>
2022-05-11 14:25:45 -0300

    lint

--- recommender-system-starter/src/recommender_system_starter/pipelines/preprocessing/pipeline.py
+++ recommender-system-starter/src/recommender_system_starter/pipelines/preprocessing/pipeline.py
@@ -9,24 +9,26 @@ from .nodes import preprocess, split_train_test
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([
-        node(
-            func=preprocess,
-            inputs={
-                "data":"master_table",
-                "user_column": "params:user_column",
-                "item_column": "params:item_column",
-                "rating_column": "params:rating_column",
-            },
-            outputs="preprocessed_data"
-        ),
-        node(
-            func=split_train_test,
-            inputs={
-                "data":"preprocessed_data",
-                "user_column": "params:user_column",
-                "test_size": "params:test_size",
-            },
-            outputs=["train_data", "test_data"]
-        )
-    ])
+    return pipeline(
+        [
+            node(
+                func=preprocess,
+                inputs={
+                    "data": "master_table",
+                    "user_column": "params:user_column",
+                    "item_column": "params:item_column",
+                    "rating_column": "params:rating_column",
+                },
+                outputs="preprocessed_data",
+            ),
+            node(
+                func=split_train_test,
+                inputs={
+                    "data": "preprocessed_data",
+                    "user_column": "params:user_column",
+                    "test_size": "params:test_size",
+                },
+                outputs=["train_data", "test_data"],
+            ),
+        ]
+    )
